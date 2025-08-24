5fb51db4ca0521af45b026db60e7ccf0515b23ab
emilianocrause <emilianocrause@gmail.com>
2025-08-24 18:50:53 -0300

    Se soluciona el burbujeo de las alertas entre el button y el div

--- solucion-click.js
+++ solucion-click.js
@@ -1,3 +1,20 @@
-document.getElementById("btnSaludar").addEventListener("click", function(event) {
-    event.stopPropagation();
+document.addEventListener("DOMContentLoaded", function() {
+    const btnSaludar = document.getElementById("btnSaludar");
+
+    const contenedor = document.createElement('div');
+    contenedor.style.padding = '5px';
+    contenedor.style.backgroundColor = 'green';
+    contenedor.style.display = 'inline-block';
+
+    btnSaludar.parentNode.insertBefore(contenedor, btnSaludar);
+    contenedor.appendChild(btnSaludar);
+
+    btnSaludar.addEventListener("click", function(event) {
+        event.stopPropagation();
+        alert("¡Hola!");
+    });
+
+    contenedor.addEventListener('click', function(){
+        alert('Hola! Soy el div');
+    });
 });
